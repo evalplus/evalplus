e461fe28fce85deef4d7031d2a16b5e57ed0d218
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-16 00:01:12 +0800

    feat(groundtruth): 040

--- groundtruth/040_triples_sum_to_zero.py
+++ groundtruth/040_triples_sum_to_zero.py
@@ -17,7 +17,12 @@ def triples_sum_to_zero(l: list):
     >>> triples_sum_to_zero([1])
     False
     """
-    pass
+    for i in range(len(l)):
+        for j in range(len(l)):
+            for k in range(len(l)):
+                if i != j and i != k and l[i] + l[j] + l[k] == 0:
+                    return True
+    return False
 
 
 
