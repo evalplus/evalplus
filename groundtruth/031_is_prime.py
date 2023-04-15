19aef80c0190b7b4791c584c68f5478649cf950b
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-15 23:58:55 +0800

    feat(groundtruth): 031

--- groundtruth/031_is_prime.py
+++ groundtruth/031_is_prime.py
@@ -17,8 +17,13 @@ def is_prime(n):
     >>> is_prime(1)
     False
     """
-    pass
-
+    if n <= 1: return False
+    n_sqrt = 1
+    while n_sqrt ** 2 < n: n_sqrt += 1
+    for i in range(2, n_sqrt + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 METADATA = {}
