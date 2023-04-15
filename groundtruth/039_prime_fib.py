2df65055ac8cdff3634575cab1e66d81e7430430
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-16 00:01:05 +0800

    feat(groundtruth): 039

--- groundtruth/039_prime_fib.py
+++ groundtruth/039_prime_fib.py
@@ -14,7 +14,44 @@ def prime_fib(n: int):
     >>> prime_fib(5)
     89
     """
-    pass
+    import random
+
+    def miller_rabin(n, k=10):
+        """Test if n is prime using the Miller-Rabin primality test."""
+        if n < 2:
+            return False
+        if n == 2 or n == 3:
+            return True
+        if n % 2 == 0:
+            return False
+
+        r = 0
+        d = n - 1
+        while d % 2 == 0:
+            r += 1
+            d //= 2
+
+        for _ in range(k):
+            a = random.randint(2, n - 2)
+            x = pow(a, d, n)
+            if x == 1 or x == n - 1:
+                continue
+            for _ in range(r - 1):
+                x = pow(x, 2, n)
+                if x == n - 1:
+                    break
+            else:
+                return False
+
+        return True
+
+    c_prime = 0
+    a, b = 0, 1
+    while c_prime < n:
+        a, b = b, a + b
+        if miller_rabin(b):
+            c_prime += 1
+    return b
 
 
 
