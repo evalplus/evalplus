001c9a6638a18196e80f8305fbe572009e7a2b35
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-15 23:58:37 +0800

    feat(groundtruth): 029

--- groundtruth/029_filter_by_prefix.py
+++ groundtruth/029_filter_by_prefix.py
@@ -8,7 +8,7 @@ def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
     >>> filter_by_prefix(['abc', 'bcd', 'cde', 'array'], 'a')
     ['abc', 'array']
     """
-    pass
+    return list(filter(lambda x: x.startswith(prefix), strings))
 
 
 
