02033155c9141a23a4d88ceb95c97f46228b0618
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-15 14:44:52 +0800

    feat(groundtruth): 019

--- groundtruth/019_sort_numbers.py
+++ groundtruth/019_sort_numbers.py
@@ -8,9 +8,10 @@ def sort_numbers(numbers: str) -> str:
     >>> sort_numbers('three one five')
     'one three five'
     """
-    pass
+    to_int = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
-
+    if numbers == "": return ""
+    return " ".join(sorted(numbers.split(" "), key=lambda n: to_int[n]))
 
 METADATA = {
     'author': 'jt',
