e0ed53462ce65ed98be13554344a1c367fe2ee99
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-15 23:58:44 +0800

    feat(groundtruth): 030

--- groundtruth/030_get_positive.py
+++ groundtruth/030_get_positive.py
@@ -7,7 +7,7 @@ def get_positive(l: list):
     >>> get_positive([5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10])
     [5, 3, 2, 3, 9, 123, 1]
     """
-    pass
+    return list(filter(lambda x: x > 0, l))
 
 
 
