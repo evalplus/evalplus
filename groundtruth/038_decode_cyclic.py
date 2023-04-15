744215e4a3078c9998a25c5f855b10865321446a
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-16 00:00:59 +0800

    feat(groundtruth): 038

--- groundtruth/038_decode_cyclic.py
+++ groundtruth/038_decode_cyclic.py
@@ -15,7 +15,9 @@ def decode_cyclic(s: str):
     """
     takes as input string encoded with encode_cyclic function. Returns decoded string.
     """
-    pass
+    groups = [s[(3 * i):min((3 * i + 3), len(s))] for i in range((len(s) + 2) // 3)]
+    groups = [(group[2] + group[:2]) if len(group) == 3 else group for group in groups]
+    return "".join(groups)
 
 
 
