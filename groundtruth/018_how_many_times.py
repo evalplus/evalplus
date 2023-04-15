ef71cce2f3bbb85fae192f887e90990ef8a4b238
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-15 14:44:46 +0800

    feat(groundtruth): 018

--- groundtruth/018_how_many_times.py
+++ groundtruth/018_how_many_times.py
@@ -9,7 +9,11 @@ def how_many_times(string: str, substring: str) -> int:
     >>> how_many_times('aaaa', 'aa')
     3
     """
-    pass
+    occurences = 0
+    for i in range(len(string)):
+        if string[i:].startswith(substring):
+            occurences += 1
+    return occurences
 
 
 
