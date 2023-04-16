43794551ae18756d6067e0d2dafea0d086c759d5
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 00:35:58 +0800

    feat(groundtruth): 043

--- groundtruth/043_pairs_sum_to_zero.py
+++ groundtruth/043_pairs_sum_to_zero.py
@@ -16,7 +16,11 @@ def pairs_sum_to_zero(l):
     >>> pairs_sum_to_zero([1])
     False
     """
-    pass
+    for i in range(len(l)):
+        for j in range(len(l)):
+            if i != j and l[i] + l[j] == 0:
+                return True
+    return False
 
 
 
