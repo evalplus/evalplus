e6a929b37f719f9fc2fd1a6ff40cea6c1d48070d
steven <stevenxia990430@gmail.com>
2023-04-15 20:48:46 -0500

    feat: added pass@k

--- fuzz_eval/evaluation/evaluate.py
+++ fuzz_eval/evaluation/evaluate.py
@@ -1,6 +1,10 @@
 import argparse
+import glob
+import itertools
 import multiprocessing
-from typing import Dict, List
+from typing import Dict, List, Union
+
+import numpy as np
 
 from fuzz_eval.evaluation.evaluate_helpers import (
     TimeoutException,
@@ -10,6 +14,35 @@ from fuzz_eval.evaluation.evaluate_helpers import (
     time_limit,
 )
 from fuzz_eval.utils import get_fuzz_eval
+
+
+# unbiased estimator from https://github.com/openai/human-eval
+def estimate_pass_at_k(
+    num_samples: Union[int, List[int], np.ndarray],
+    num_correct: Union[List[int], np.ndarray],
+    k: int,
+) -> np.ndarray:
+    """
+    Estimates pass@k of each problem and returns them in an array.
+    """
+
+    def estimator(n: int, c: int, k: int) -> float:
+        """
+        Calculates 1 - comb(n - c, k) / comb(n, k).
+        """
+        if n - c < k:
+            return 1.0
+        return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
+
+    if isinstance(num_samples, int):
+        num_samples_it = itertools.repeat(num_samples, len(num_correct))
+    else:
+        assert len(num_samples) == len(num_correct)
+        num_samples_it = iter(num_samples)
+
+    return np.array(
+        [estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)]
+    )
 
 
 def construct_inputs_sig(inputs: list) -> str:
@@ -64,7 +97,23 @@ def execute(code: str, inputs: List, signature: str) -> str:
     return result[0]
 
 
+def evaluate_files(files: List[str], inputs, outputs, entry_point: str) -> List[str]:
+    suc_files = []
+    for file in files:
+        code = open(file, "r").read()
+        suc = True
+        for input, output in zip(inputs, outputs):
+            o = execute(code, input, entry_point)
+            if o != output:
+                suc = False
+                break
+        if suc:
+            suc_files.append(file)
+    return suc_files
+
+
 def evaluate(args, problems):
+    base_total, base_correct = [], []
     for problem in problems:
         if (
             problem["reference"].strip().startswith("pass")
@@ -83,10 +132,28 @@ def evaluate(args, problems):
             assert o != "timed out" and o != "thrown exception"
             gd_results.append(o)
 
+        gen_files = glob.glob(args.r_folder + f"/{p_name}/*.py")
+        base_suc_files = evaluate_files(
+            gen_files, problem["base_input"], gd_results, problem["entry_point"]
+        )
+        print(f"Total Gen: {len(gen_files)}, Base Success Files: {len(base_suc_files)}")
+
+        base_total.append(len(gen_files))
+        base_correct.append(len(base_suc_files))
+
         if args.more_eval:
             # where we would apply the additional testcases.
             raise NotImplementedError("More evaluation has not been implemented")
-    pass
+
+    # Calculate pass@k.
+    total = np.array(base_total)
+    correct = np.array(base_correct)
+    pass_at_k = {
+        f"pass@{k}": estimate_pass_at_k(total, correct, k).mean()
+        for k in [1, 10, 100]
+        if (total >= k).all()
+    }
+    print(pass_at_k)
 
 
 def main():
