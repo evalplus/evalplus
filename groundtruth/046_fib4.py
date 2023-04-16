798fa6ea0f3f42648a7dc194cfde35a3be7790cd
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 00:44:32 +0800

    feat(groundtruth): 046

--- groundtruth/046_fib4.py
+++ groundtruth/046_fib4.py
@@ -15,8 +15,21 @@ def fib4(n: int):
     >>> fib4(7)
     14
     """
-    pass
-
+    assert n >= 0, "invalid inputs"
+    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 0
+    else:
+        a, b, c, d = 0, 0, 2, 0
+        for i in range(4, n + 1):
+            a, b, c, d = b, c, d, a + b + c + d
+        return d
 
 
 METADATA = {}
