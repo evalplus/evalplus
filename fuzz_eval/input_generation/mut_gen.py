31cff43b6731a18250ad87ea330b181aa2a88618
steven <stevenxia990430@gmail.com>
2023-04-16 01:09:32 -0500

    feat: initial mutation input generation

---
+++ fuzz_eval/input_generation/mut_gen.py
@@ -0,0 +1,104 @@
+import copy
+import random
+import string
+from typing import List
+
+
+class MutateGen(object):
+    def __init__(self, inputs: List):
+        self.seed_pool = inputs
+        self.seed_hash = set([hash(str(x)) for x in self.seed_pool])
+        self.new_inputs = []
+
+    def seed_selection(self):
+        # random for now.
+        return random.choice(self.seed_pool)
+
+    def mutate(self, seed) -> List:
+
+        new_input = copy.deepcopy(seed)
+
+        while hash(str(new_input)) == hash(str(seed)):
+            strategy = random.randint(0, 7)
+            if len(new_input) == 0:
+                break
+            element = random.randint(0, len(new_input) - 1)
+            # increment
+            if strategy == 0:
+                if type(new_input[element]) in {int, float}:
+                    new_input[element] += 1
+            # decrement
+            elif strategy == 1:
+                if type(new_input[element]) in {int, float}:
+                    new_input[element] -= 1
+            # negative
+            elif strategy == 2:
+                if type(new_input[element]) in {int, float}:
+                    new_input[element] = -new_input[element]
+            # string bitflip
+            # elif strategy == 3:
+            #     if type(new_input[element]) in {str}:
+            #         index = random.randint(0, len(new_input[element])-1)
+            #         new_input[element][index] = random.choice(string.ascii_letters)
+            # string add character
+            elif strategy == 4:
+                if type(new_input[element]) in {str}:
+                    index = random.randint(0, len(new_input[element]))
+                    if index == len(new_input[element]):
+                        new_input[element] = new_input[element] + random.choice(
+                            string.ascii_letters
+                        )
+                    else:
+                        new_input[element] = (
+                            new_input[element][:index]
+                            + random.choice(string.ascii_letters)
+                            + new_input[element][index:]
+                        )
+            # string remove character.
+            elif strategy == 5:
+                if type(new_input[element]) in {str}:
+                    index = random.randint(0, len(new_input[element]))
+                    if index == len(new_input[element]):
+                        break
+                    else:
+                        s = str(
+                            new_input[element][: index - 1] + new_input[element][index:]
+                        )
+                        new_input[element] = s
+            # list add element
+            elif strategy == 6:
+                if type(new_input[element]) in {list}:
+                    if len(new_input[element]) == 0:
+                        break
+                    index = random.randint(0, len(new_input[element]) - 1)
+                    if type(new_input[element][index]) in {int, float}:
+                        new_input[element].append(
+                            type(new_input[element][index])(random.randrange(0, 100))
+                        )
+                    if type(new_input[element][index]) in {str}:
+                        new_input[element].append(
+                            "".join(
+                                random.choices(
+                                    string.ascii_letters, k=random.randint(1, 7)
+                                )
+                            )
+                        )
+            # list remove element.
+            elif strategy == 7:
+                if type(new_input[element]) in {list}:
+                    if len(new_input[element]) == 0:
+                        break
+                    index = random.randint(0, len(new_input[element]) - 1)
+                    new_input[element].pop(index)
+
+        return new_input
+
+    def generate(self, num: int):
+        while len(self.new_inputs) < num:
+            seed = self.seed_selection()
+            new_input = self.mutate(seed)
+            if hash(str(new_input)) not in self.seed_hash:
+                self.seed_pool.append(new_input)
+                self.seed_hash.add(hash(str(new_input)))
+                self.new_inputs.append(new_input)
+        return self.new_inputs
