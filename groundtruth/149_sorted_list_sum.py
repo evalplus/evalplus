834d66cdeaf158c74c3b58d9b59942b097e27630
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:48 +0800

    feat(groundtruth): 149

--- groundtruth/149_sorted_list_sum.py
+++ groundtruth/149_sorted_list_sum.py
@@ -14,7 +14,12 @@ def sorted_list_sum(lst):
     assert list_sort(["aa", "a", "aaa"]) => ["aa"]
     assert list_sort(["ab", "a", "aaa", "cd"]) => ["ab", "cd"]
     """
-    pass
+    from functools import cmp_to_key
+    def cmp(s: str, t: str):
+        if len(s) != len(t):
+            return len(s) - len(t)
+        return -1 if s < t else 1
+    return sorted(list(filter(lambda s: len(s) % 2 == 0, lst)), key=cmp_to_key(cmp))
 
 def check(candidate):
 
