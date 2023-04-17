b561c99ed3540e3d00c7f962aa94727731123f6a
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 16:43:31 +0800

    feat(groundtruth): 106

--- groundtruth/106_f.py
+++ groundtruth/106_f.py
@@ -8,7 +8,19 @@ def f(n):
     Example:
     f(5) == [1, 2, 6, 24, 15]
     """
-    pass
+    assert n >= 0, "invalid inputs" # $_CONTRACT_$
+
+    if n == 0: return []
+    if n == 1: return [1]
+    if n == 2: return [1, 2]
+
+    ans = [1, 2]
+    for i in range(3, n + 1):
+        if i % 2 == 1:
+            ans.append(ans[-2] + (i - 1) + i)
+        else:
+            ans.append(ans[-2] * (i - 1) * i)
+    return ans
 
 def check(candidate):
 
