f135a96e794f3b14a483cd95c78e6c359bc55539
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:46 +0800

    feat(groundtruth): 143

--- groundtruth/143_words_in_sentence.py
+++ groundtruth/143_words_in_sentence.py
@@ -19,7 +19,13 @@ def words_in_sentence(sentence):
         * 1 <= len(sentence) <= 100
         * sentence contains only letters
     """
-    pass
+    assert 1 <= len(sentence) <= 100, "invalid inputs" # $_CONTRACT_$
+    assert "  " not in sentence, "invalid inputs" # $_CONTRACT_$
+    assert all(word.isalpha() for word in sentence.split(" ")), "invalid inputs" # $_CONTRACT_$
+
+    def is_prime(a):
+        return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1)))
+    return " ".join(list(filter(lambda word: is_prime(len(word)), sentence.split(" "))))
 
 def check(candidate):
 
