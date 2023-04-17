4a1a87a584ece6410dc8f51ca16867af68df9ae8
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 15:42:25 +0800

    feat(groundtruth): 093

--- groundtruth/093_encode.py
+++ groundtruth/093_encode.py
@@ -13,7 +13,21 @@ def encode(message):
     >>> encode('This is a message')
     'tHKS KS C MGSSCGG'
     """
-    pass
+    assert all(ch == " " or ch.isalpha() for ch in message), "invalid inputs" # $_CONTRACT_$
+
+    def switch_case(ch):
+        if ord("A") <= ord(ch) <= ord("Z"):
+            return chr(ord(ch) + 32)
+        elif ord("a") <= ord(ch) <= ord("z"):
+            return chr(ord(ch) - 32)
+        else:
+            return ch
+    
+    def vowel_change(ch):
+        return ch if ch not in "aeiouAEIOU" else chr(ord(ch) + 2)
+    
+    m = "".join(map(switch_case, message))
+    return "".join(map(vowel_change, m))
 
 def check(candidate):
 
