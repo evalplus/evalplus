1a38cbb26db644adafbf7f9e76c6b1d5d5fcc190
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:41 +0800

    feat(groundtruth): 118

--- groundtruth/118_get_closest_vowel.py
+++ groundtruth/118_get_closest_vowel.py
@@ -14,7 +14,14 @@ def get_closest_vowel(word):
     get_closest_vowel("quick") ==> ""
     get_closest_vowel("ab") ==> ""
     """
-    pass
+    assert word == "" or word.isalpha(), "invalid inputs" # $_CONTRACT_$
+
+    def is_vowel(ch: str) -> bool:
+        return ch in "aeiouAEIOU"
+    for i in range(len(word) - 2, 0, -1):
+        if is_vowel(word[i]) and not is_vowel(word[i-1]) and not is_vowel(word[i+1]):
+            return word[i]
+    return ""
 
 def check(candidate):
 
