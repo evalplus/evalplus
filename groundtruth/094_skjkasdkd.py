78210e125205c48c321ddd903d86fcdd02b03d08
ganler <jaway.liu@gmail.com>
2023-04-17 15:14:26 -0500

    fix: contract independence at 094

--- groundtruth/094_skjkasdkd.py
+++ groundtruth/094_skjkasdkd.py
@@ -16,6 +16,8 @@ def skjkasdkd(lst):
         return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1))) # $_CONTRACT_$
     assert any(is_prime(x) for x in lst), "invalid inputs" # $_CONTRACT_$
 
+    def is_prime(a):
+        return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1)))
     sorted_list = sorted(lst)[::-1]
     for x in sorted_list:
         if is_prime(x):
