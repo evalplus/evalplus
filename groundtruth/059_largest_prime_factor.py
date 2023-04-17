dfa03b61e9bb21597ad0d10677cc7ad834c3a36b
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 11:22:02 +0800

    feat(groundtruth): 059

--- groundtruth/059_largest_prime_factor.py
+++ groundtruth/059_largest_prime_factor.py
@@ -7,8 +7,18 @@ def largest_prime_factor(n: int):
     >>> largest_prime_factor(2048)
     2
     """
-    pass
+    def is_prime(a): # $_CONTRACT_$
+        return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1))) # $_CONTRACT_$
+    assert n > 1 and not is_prime(n), "invalid inputs" # $_CONTRACT_$
 
+    isprime = [True] * (n + 1)
+    for i in range(2, n + 1):
+        if isprime[i]:
+            for j in range(i + i, n, i):
+                isprime[j] = False
+    for i in range(n - 1, 0, -1):
+        if isprime[i] and n % i == 0:
+            return i
 
 
 METADATA = {}
