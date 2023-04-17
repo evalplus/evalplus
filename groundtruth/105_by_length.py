c0e128cc16abed4e38b747b65b0b705e0e0c97cf
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 16:38:27 +0800

    feat(groundtruth): 105

--- groundtruth/105_by_length.py
+++ groundtruth/105_by_length.py
@@ -21,7 +21,30 @@ def by_length(arr):
             -> reverse arr -> [55, 1, -1]
       return = ['One']
     """
-    pass
+    def to_word(x: int) -> str:
+      if x == 1:
+        return "One"
+      elif x == 2:
+        return "Two"
+      elif x == 3:
+        return "Three"
+      elif x == 4:
+        return "Four"
+      elif x == 5:
+        return "Five"
+      elif x == 6:
+        return "Six"
+      elif x == 7:
+        return "Seven"
+      elif x == 8:
+        return "Eight"
+      else:
+        return "Nine"
+    sorted_list, ans = sorted(arr)[::-1], []
+    for x in sorted_list:
+      if 1 <= x <= 9:
+        ans.append(to_word(x))
+    return ans
 
 def check(candidate):
 
