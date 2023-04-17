0cef43026a2760178f3283f189cb691f29e567d7
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 11:30:29 +0800

    feat(groundtruth): 063

--- groundtruth/063_fibfib.py
+++ groundtruth/063_fibfib.py
@@ -14,8 +14,16 @@ def fibfib(n: int):
     >>> fibfib(8)
     24
     """
-    pass
+    assert n >= 0, "invalid inputs" # $_CONTRACT_$
 
+    if n == 0 or n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    a, b, c = 0, 0, 1
+    for _ in range(3, n + 1):
+        a, b, c = b, c, a + b + c
+    return c
 
 
 METADATA = {}
