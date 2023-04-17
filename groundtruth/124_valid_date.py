119d0cf0bd69b575db5ea3414a73fe2d47f236da
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:42 +0800

    feat(groundtruth): 124

--- groundtruth/124_valid_date.py
+++ groundtruth/124_valid_date.py
@@ -19,7 +19,15 @@ def valid_date(date):
 
     valid_date('06/04/2020') => False
     """
-    pass
+    days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if len(date) != 10: return False
+    if date[2] != "-" or date[5] != "-": return False
+    m, d, y = date[:2], date[3:5], date[6:]
+    if not m.isdigit() or not d.isdigit() or not y.isdigit(): return False
+    m, d = int(m), int(d)
+    if not 1 <= m <= 12: return False
+    if not 1 <= d <= days[m-1]: return False
+    return True
 
 def check(candidate):
 
