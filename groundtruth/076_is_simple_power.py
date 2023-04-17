393b51fca70e970f7f31a9f2466afd1da3cfcd6b
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 14:27:06 +0800

    feat(groundtruth): 076

--- groundtruth/076_is_simple_power.py
+++ groundtruth/076_is_simple_power.py
@@ -11,7 +11,15 @@ def is_simple_power(x, n):
     is_simple_power(3, 1) => false
     is_simple_power(5, 3) => false
     """
-    pass
+    if x == 1: return True
+    if n == 0: return x == 0
+    if n == 1: return x == 1
+    if n == -1: return abs(x) == 1
+    p = n
+    while abs(p) <= abs(x):
+        if p == x: return True
+        p = p * n
+    return False
 
 def check(candidate):
 
