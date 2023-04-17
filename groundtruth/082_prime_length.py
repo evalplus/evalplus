a596e171dbff94a32a6144e240a95f613f567de4
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 15:01:47 +0800

    feat(groundtruth): 082

--- groundtruth/082_prime_length.py
+++ groundtruth/082_prime_length.py
@@ -8,7 +8,10 @@ def prime_length(string):
     prime_length('kittens') == True
     prime_length('orange') == False
     """
-    pass
+    def is_prime(a):
+        return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1)))
+
+    return is_prime(len(string))
 
 def check(candidate):
 
