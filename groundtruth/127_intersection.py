8dfc594f27ec81f2252df0dd968a2e14e99b751a
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:43 +0800

    feat(groundtruth): 127

--- groundtruth/127_intersection.py
+++ groundtruth/127_intersection.py
@@ -19,7 +19,14 @@ def intersection(interval1, interval2):
     intersection((-1, 1), (0, 4)) ==> "NO"
     intersection((-3, -1), (-5, 5)) ==> "YES"
     """
-    pass
+    assert len(interval1) == 2 and interval1[0] <= interval1[1], "invalid inputs" # $_CONTRACT_$
+    assert len(interval2) == 2 and interval2[0] <= interval2[1], "invalid inputs" # $_CONTRACT_$
+
+    def is_prime(a):
+        return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1)))
+    if interval1[0] > interval2[0]: interval1, interval2 = interval2, interval1
+    l, r = interval2[0], min(interval1[1], interval2[1])
+    return "YES" if is_prime(r - l) else "NO"
 
 def check(candidate):
 
