541bb2824b8c9e9af67ef8fcdcd3be88a90e7c2c
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 14:12:00 +0800

    feat(groundtruth): 070

--- groundtruth/070_strange_sort_list.py
+++ groundtruth/070_strange_sort_list.py
@@ -10,7 +10,15 @@ def strange_sort_list(lst):
     strange_sort_list([5, 5, 5, 5]) == [5, 5, 5, 5]
     strange_sort_list([]) == []
     '''
-    pass
+    sorted_list = sorted(lst)
+    ans, i, j = [], 0, len(sorted_list) - 1
+    while i < j:
+        ans.append(sorted_list[i])
+        ans.append(sorted_list[j])
+        i += 1
+        j -= 1
+    if i == j: ans.append(sorted_list[i])
+    return ans
 
 def check(candidate):
 
