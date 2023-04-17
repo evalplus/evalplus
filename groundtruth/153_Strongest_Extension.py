af30c4d52019d42f0104b4f65fbdb93deb0a370c
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:49 +0800

    feat(groundtruth): 153

--- groundtruth/153_Strongest_Extension.py
+++ groundtruth/153_Strongest_Extension.py
@@ -16,7 +16,18 @@ def Strongest_Extension(class_name, extensions):
     Example:
     for Strongest_Extension('my_class', ['AA', 'Be', 'CC']) == 'my_class.AA'
     """
-    pass
+    assert len(extensions) > 0, "invalid inputs" # $_CONTRACT_$
+
+    def strength(s: str) -> int:
+        CAP, SM = 0, 0
+        for ch in s:
+            if ch.isupper(): CAP += 1
+            if ch.islower(): SM += 1
+        return CAP - SM
+    max_strength = max(map(strength, extensions))
+    for e in extensions:
+        if strength(e) == max_strength:
+            return class_name + "." + e
 
 def check(candidate):
 
