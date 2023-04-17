8e87d57f1dc23d193857268e798e714f33142200
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:49 +0800

    feat(groundtruth): 152

--- groundtruth/152_compare.py
+++ groundtruth/152_compare.py
@@ -14,7 +14,9 @@ def compare(game,guess):
     compare([1,2,3,4,5,1],[1,2,3,4,2,-2]) -> [0,0,0,0,3,3]
     compare([0,5,0,0,0,4],[4,1,1,0,0,-2]) -> [4,4,1,0,0,6]
     """
-    pass
+    assert len(game) == len(guess), "invalid inputs" # $_CONTRACT_$
+
+    return [abs(game[i] - guess[i]) for i in range(len(game))]
 
 def check(candidate):
 
