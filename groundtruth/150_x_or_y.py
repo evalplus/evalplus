526278dd609ea694a3f57943463263405b9a4613
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:48 +0800

    feat(groundtruth): 150

--- groundtruth/150_x_or_y.py
+++ groundtruth/150_x_or_y.py
@@ -8,7 +8,9 @@ def x_or_y(n, x, y):
     for x_or_y(15, 8, 5) == 5
     
     """
-    pass
+    def is_prime(a):
+        return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1)))
+    return x if is_prime(n) else y
 
 def check(candidate):
 
