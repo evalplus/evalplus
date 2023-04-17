d8e2704b43abed7e669600d524e64c82998fefa4
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:46 +0800

    feat(groundtruth): 141

--- groundtruth/141_file_name_check.py
+++ groundtruth/141_file_name_check.py
@@ -13,7 +13,14 @@ def file_name_check(file_name):
     file_name_check("example.txt") # => 'Yes'
     file_name_check("1example.dll") # => 'No' (the name should start with a latin alphapet letter)
     """
-    pass
+    if len(list(filter(lambda ch: ch.isdigit(), file_name))) > 3:
+        return "No"
+    f_list = file_name.split(".")
+    if len(f_list) != 2: return "No"
+    if len(f_list[0]) == 0: return "No"
+    if not f_list[0][0].isalpha(): return "No"
+    if f_list[1] not in ["txt", "exe", "dll"]: return "No"
+    return "Yes"
 
 def check(candidate):
 
