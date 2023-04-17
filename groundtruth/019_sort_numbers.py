eb2a5c96751d509281a27ffb23e8800809deeec5
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 10:34:12 +0800

    refactor(contract): 019

--- groundtruth/019_sort_numbers.py
+++ groundtruth/019_sort_numbers.py
@@ -8,10 +8,11 @@ def sort_numbers(numbers: str) -> str:
     >>> sort_numbers('three one five')
     'one three five'
     """
+    assert numbers == "" or all(map(lambda x: x in ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], numbers.split(" "))), "invalid inputs"
+    
     to_int = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
     if numbers == "": return ""
-    assert all(map(lambda x: x in to_int, numbers.split(" "))), "invalid inputs"
     return " ".join(sorted(numbers.split(" "), key=lambda n: to_int[n]))
 
 METADATA = {
