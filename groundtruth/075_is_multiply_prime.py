fd5801d38bb20956475407ff85144f336938af80
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 14:23:42 +0800

    feat(groundtruth): 075

--- groundtruth/075_is_multiply_prime.py
+++ groundtruth/075_is_multiply_prime.py
@@ -7,7 +7,18 @@ def is_multiply_prime(a):
     is_multiply_prime(30) == True
     30 = 2 * 3 * 5
     """
-    pass
+    if a <= 1: return False
+    isprime = [True] * (a + 1)
+    for i in range(2, a + 1):
+        if isprime[i]:
+            for j in range(i + i, a + 1, i):
+                isprime[j] = False
+    cnt, tmp = 0, a
+    for i in range(2, a + 1):
+        while isprime[i] and tmp % i == 0:
+            tmp //= i
+            cnt += 1
+    return cnt == 3
 
 def check(candidate):
 
