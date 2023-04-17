e24075c0a45af8268cd9a2ebdff410dd7b429ab7
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:49 +0800

    feat(groundtruth): 156

--- groundtruth/156_int_to_mini_roman.py
+++ groundtruth/156_int_to_mini_roman.py
@@ -10,7 +10,17 @@ def int_to_mini_roman(number):
     >>> int_to_mini_roman(152) == 'clii'
     >>> int_to_mini_roman(426) == 'cdxxvi'
     """
-    pass
+    assert 1 <= number <= 1000, "invalid inputs" # $_CONTRACT_$
+
+    m = ["", "m"]
+    c = ["", "c", "cc", "ccc", "cd", "d", "dc", "dcc", "dccc", "cm"]
+    x = ["", "x", "xx", "xxx", "xl", "l", "lx", "lxx", "lxxx", "xc"]
+    i = ["", "i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix"]
+    thousands = m[number // 1000]
+    hundreds = c[(number % 1000) // 100]
+    tens = x[(number % 100) // 10]
+    ones = i[number % 10]
+    return thousands + hundreds + tens + ones
 
 def check(candidate):
 
