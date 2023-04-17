3c6e1d1bed76d02d16ab2ea202f723d9938b6c04
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:50 +0800

    feat(groundtruth): 158

--- groundtruth/158_find_max.py
+++ groundtruth/158_find_max.py
@@ -9,7 +9,14 @@ def find_max(words):
     find_max(["name", "enam", "game"]) == "enam"
     find_max(["aaaaaaa", "bb" ,"cc"]) == ""aaaaaaa"
     """
-    pass
+    assert len(words) > 0, "invalid inputs" # $_CONTRACT_$
+
+    mx_ch_cnt, ans = 0, ""
+    for word in words:
+        ch_cnt = len(set(word)) 
+        if ch_cnt > mx_ch_cnt or (ch_cnt == mx_ch_cnt and word < ans):
+            mx_ch_cnt, ans = ch_cnt, word
+    return ans
 
 def check(candidate):
 
