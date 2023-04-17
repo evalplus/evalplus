dafa14538e0fa2106d2f7932ffbca3de92b104c2
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 15:36:06 +0800

    feat(groundtruth): 092

--- groundtruth/092_any_int.py
+++ groundtruth/092_any_int.py
@@ -17,7 +17,8 @@ def any_int(x, y, z):
 
     
     '''
-    pass
+    if type(x) != int or type(y) != int or type(z) != int: return False
+    return x == y + z or y == x + z or z == y + x
 
 def check(candidate):
 
