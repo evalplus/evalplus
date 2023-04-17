c4e2e1092f9c11bf3f271654863a595180cc443d
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:49 +0800

    feat(groundtruth): 155

--- groundtruth/155_even_odd_count.py
+++ groundtruth/155_even_odd_count.py
@@ -6,7 +6,11 @@ def even_odd_count(num):
         even_odd_count(-12) ==> (1, 1)
         even_odd_count(123) ==> (1, 2)
     """
-    pass
+    even, odd = 0, 0
+    for ch in str(num):
+        if ch in "02468": even += 1
+        if ch in "13579": odd += 1
+    return even, odd
 
 def check(candidate):
 
