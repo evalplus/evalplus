b017d8d54bc80628eab6a832da612c79881d7468
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:51 +0800

    feat(groundtruth): 162

--- groundtruth/162_string_to_md5.py
+++ groundtruth/162_string_to_md5.py
@@ -6,7 +6,11 @@ def string_to_md5(text):
 
     >>> string_to_md5('Hello world') == '3e25960a79dbc69b674cd4ec67a72c62'
     """
-    pass
+    if text == "": return None
+    import hashlib
+    m = hashlib.md5()
+    m.update(text.encode("utf-8"))
+    return m.hexdigest()
 
 def check(candidate):
 
