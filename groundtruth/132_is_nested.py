b18a77ba5adc1ecfa10204a1a4aad2ffeb6ab004
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:44 +0800

    feat(groundtruth): 132

--- groundtruth/132_is_nested.py
+++ groundtruth/132_is_nested.py
@@ -12,7 +12,22 @@ def is_nested(string):
     is_nested('[[][]]') ➞ True
     is_nested('[[]][[') ➞ True
     '''
-    pass
+    assert all(ch == "[" or ch == "]" for ch in string), "invalid inputs" # $_CONTRACT_$
+
+    for i in range(len(string)):
+        if string[i] == "]": continue
+        cnt, max_nest = 0, 0
+        for j in range(i, len(string)):
+            if string[j] == "[":
+                cnt += 1
+            else:
+                cnt -= 1
+            max_nest = max(max_nest, cnt)
+            if cnt == 0:
+                if max_nest >= 2:
+                    return True
+                break
+    return False
 
 def check(candidate):
 
