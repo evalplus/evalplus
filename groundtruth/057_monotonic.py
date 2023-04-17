9f823ac5705b8ccbc0c402585f0336d26136b737
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 11:08:20 +0800

    feat(groundtruth): 057

--- groundtruth/057_monotonic.py
+++ groundtruth/057_monotonic.py
@@ -9,7 +9,11 @@ def monotonic(l: list):
     >>> monotonic([4, 1, 0, -10])
     True
     """
-    pass
+    inc, dec = True, True
+    for i in range(len(l) - 1):
+        if l[i] > l[i + 1]: inc = False
+        if l[i] < l[i + 1]: dec = False
+    return inc or dec
 
 
 
