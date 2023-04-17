c611638d19d55fd5955f095743f8e6da7f3221a7
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:44 +0800

    feat(groundtruth): 134

--- groundtruth/134_check_if_last_char_is_a_letter.py
+++ groundtruth/134_check_if_last_char_is_a_letter.py
@@ -12,7 +12,9 @@ def check_if_last_char_is_a_letter(txt):
     check_if_last_char_is_a_letter("apple pi e ") ➞ False
     check_if_last_char_is_a_letter("") ➞ False 
     '''
-    pass
+    if len(txt) == 0: return False
+    if len(txt) == 1: return txt.isalpha()
+    return txt[-1].isalpha() and txt[-2] == " "
 
 def check(candidate):
 
