5a886ee8658dd96122b23a7711b9f9ada5d31af4
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 15:53:30 +0800

    feat(groundtruth): 098

--- groundtruth/098_count_upper.py
+++ groundtruth/098_count_upper.py
@@ -8,7 +8,11 @@ def count_upper(s):
     count_upper('abcdefg') returns 0
     count_upper('dBBE') returns 0
     """
-    pass
+    cnt = 0
+    for i in range(0, len(s), 2):
+        if s[i] in "AEIOU":
+            cnt += 1
+    return cnt
 
 def check(candidate):
 
