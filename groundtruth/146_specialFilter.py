807d9539b3c1a933d7772fa989d344fa9453cf96
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:47 +0800

    feat(groundtruth): 146

--- groundtruth/146_specialFilter.py
+++ groundtruth/146_specialFilter.py
@@ -7,7 +7,11 @@ def specialFilter(nums):
     specialFilter([15, -73, 14, -15]) => 1 
     specialFilter([33, -2, -3, 45, 21, 109]) => 2
     """
-    pass
+    ans, odd = 0, ["1", "3", "5", "7", "9"]
+    for num in nums:
+        if num > 10 and str(num)[0] in odd and str(num)[-1] in odd:
+            ans += 1
+    return ans
 
 def check(candidate):
 
