e044a04bef5616a2eab7a6aee9aaf60c54a08594
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-18 01:09:40 +0800

    feat(groundtruth): 114

--- groundtruth/114_minSubArraySum.py
+++ groundtruth/114_minSubArraySum.py
@@ -7,7 +7,15 @@ def minSubArraySum(nums):
     minSubArraySum([2, 3, 4, 1, 2, 4]) == 1
     minSubArraySum([-1, -2, -3]) == -6
     """
-    pass
+    assert len(nums) > 0, "invalid inputs" # $_CONTRACT_$
+
+    if all(x >= 0 for x in nums): return min(nums)
+    s, ans = 0, 0
+    for x in nums:
+        s += x
+        ans = min(ans, s)
+        if s >= 0: s = 0
+    return ans
 
 def check(candidate):
 
