7e160e774c26d8dc80f0651b1efecd0ac3d433e4
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-17 16:50:37 +0800

    feat(groundtruth): 108

--- groundtruth/108_count_nums.py
+++ groundtruth/108_count_nums.py
@@ -9,7 +9,16 @@ def count_nums(arr):
     >>> count_nums([-1, 11, -11]) == 1
     >>> count_nums([1, 1, 2]) == 3
     """
-    pass
+    def judge(x: int) -> int:
+        l = list(str(x))
+        if l[0] == "-":
+            l = l[1:]
+            l = list(map(int, l))
+            l[0] = -l[0]
+        else:
+            l = list(map(int, l))
+        return 1 if sum(l) > 0 else 0
+    return sum(map(judge, arr))
 
 def check(candidate):
 
