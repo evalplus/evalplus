771243103e0760ee0546023481780d8ed8d2307d
steven <stevenxia990430@gmail.com>
2023-04-21 18:24:10 -0500

    fix(bk) triples_sum_to_zero

--- groundtruth_bk/040_triples_sum_to_zero.py
+++ groundtruth_bk/040_triples_sum_to_zero.py
@@ -20,7 +20,7 @@ def triples_sum_to_zero(l: list):
     for i in range(len(l)):
         for j in range(len(l)):
             for k in range(len(l)):
-                if i != j and i != k and l[i] + l[j] + l[k] == 0:
+                if i != j and i != k and j != k and l[i] + l[j] + l[k] == 0:
                     return True
     return False
 
