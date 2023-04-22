6ad435c11029437e14e35c7e4762e67ad8ac2ab3
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-22 02:33:23 -0500

    fix(gt): is_prime

--- groundtruth_bk/031_is_prime.py
+++ groundtruth_bk/031_is_prime.py
@@ -20,7 +20,7 @@ def is_prime(n):
     if n <= 1: return False
     n_sqrt = 1
     while n_sqrt ** 2 < n: n_sqrt += 1
-    for i in range(2, n_sqrt + 1):
+    for i in range(2, min(n_sqrt + 1, n)):
         if n % i == 0:
             return False
     return True
