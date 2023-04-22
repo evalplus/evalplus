e978939d088a82e46a85339eb0c775a5379c78a9
steven <stevenxia990430@gmail.com>
2023-04-21 19:24:08 -0500

    fix(bk) double the difference

--- groundtruth_bk/151_double_the_difference.py
+++ groundtruth_bk/151_double_the_difference.py
@@ -13,7 +13,7 @@ def double_the_difference(lst):
     '''
     ans = 0
     for num in lst:
-        if num % 2 == 1 and num > 0:
+        if num % 2 == 1 and num > 0 and "." not in str(num):
             ans += num ** 2
     return ans
 
