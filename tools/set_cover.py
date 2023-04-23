363803f08ea509555402135b15c2ec86a56819d7
ganler <jaway.liu@gmail.com>
2023-04-23 13:03:11 -0500

    refactor: yao bro please use pre-commit install

--- tools/set_cover.py
+++ tools/set_cover.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 from rich.progress import track
 
 from eval_plus.utils import get_human_eval_plus, get_human_eval_plus_inputs
@@ -14,25 +15,30 @@ new_inputs = get_human_eval_plus_inputs()
 
 # One dict is super huge, so split them into separate JSON files
 def get_cover_info():
-    if os.path.exists(COV_INFO_PATH): return
+    if os.path.exists(COV_INFO_PATH):
+        return
     os.makedirs(COV_INFO_PATH)
     cover_info = {f"HumanEval_{i}": {} for i in range(164)}
     for model_path in model_paths:
-        if not model_path[-1].isdigit(): continue
+        if not model_path[-1].isdigit():
+            continue
         eval_json_path = os.path.join(LLM_HOME_PATH, model_path, "eval_results.json")
-        if not os.path.exists(eval_json_path): continue
+        if not os.path.exists(eval_json_path):
+            continue
         with open(eval_json_path, "r") as f:
             res = json.load(f)["eval"]
             for task_id, v in res.items():
                 for i_code, (status, res_list) in enumerate(v["base"]):
-                    if status == "success": continue
+                    if status == "success":
+                        continue
                     code_id = hash(v["files"][i_code])
                     for i_test, res in enumerate(res_list):
                         test_id = f"base_{i_test}"
                         if res == False:
                             cover_info[task_id].setdefault(test_id, []).append(code_id)
                 for i_code, (status, res_list) in enumerate(v["plus"]):
-                    if status == "success": continue
+                    if status == "success":
+                        continue
                     code_id = hash(v["files"][i_code])
                     for i_test, res in enumerate(res_list):
                         test_id = f"plus_{i_test}"
@@ -41,6 +47,7 @@ def get_cover_info():
     for task_id, tests in cover_info.items():
         with open(os.path.join(COV_INFO_PATH, f"{task_id}.json"), "w") as f:
             json.dump(tests, f)
+
 
 if __name__ == "__main__":
     get_cover_info()
@@ -53,7 +60,7 @@ if __name__ == "__main__":
             cover_set = set(test_cover)
             q.append((test_name, cover_set))
             U = U.union(cover_set)
-        # Greedy 
+        # Greedy
         min_cover = []
         while len(U) > 0:
             max_uncover_set, max_test_name = {}, ""
