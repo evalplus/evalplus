147844daa5938cc12a6fe5ee1db71ca4aac3a59a
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-25 11:21:37 -0500

    refactor: dont store intermediate results

--- tools/set_cover.py
+++ tools/set_cover.py
@@ -1,25 +1,22 @@
 import json
 import os
+import pickle
 
 from rich.progress import track
 
 from eval_plus.utils import get_human_eval_plus, get_human_eval_plus_inputs
 
 LLM_HOME_PATH = "/JawTitan/EvalPlus/humaneval"
-COV_INFO_PATH = "min_cov_dir"
 model_paths = os.listdir(LLM_HOME_PATH)
 
 problems = get_human_eval_plus()
 new_inputs = get_human_eval_plus_inputs()
+cover_info = {f"HumanEval_{i}": {} for i in range(164)}
 
 
 # One dict is super huge, so split them into separate JSON files
 def get_cover_info():
-    if os.path.exists(COV_INFO_PATH):
-        return
-    os.makedirs(COV_INFO_PATH)
-    cover_info = {f"HumanEval_{i}": {} for i in range(164)}
-    for model_path in model_paths:
+    for model_path in track(model_paths, description="Collecting sets..."):
         if not model_path[-1].isdigit():
             continue
         eval_json_path = os.path.join(LLM_HOME_PATH, model_path, "eval_results.json")
@@ -44,17 +41,13 @@ def get_cover_info():
                         test_id = f"plus_{i_test}"
                         if res == False:
                             cover_info[task_id].setdefault(test_id, []).append(code_id)
-    for task_id, tests in cover_info.items():
-        with open(os.path.join(COV_INFO_PATH, f"{task_id}.json"), "w") as f:
-            json.dump(tests, f)
 
 
 if __name__ == "__main__":
     get_cover_info()
-    for i in track(range(164)):
+    for i in track(range(164), description="Solving set covering..."):
         task_id = f"HumanEval_{i}"
-        with open(os.path.join(COV_INFO_PATH, f"{task_id}.json"), "r") as f:
-            tests = json.load(f)
+        tests = cover_info[task_id]
         q, U = [], set()
         for test_name, test_cover in tests.items():
             cover_set = set(test_cover)
@@ -84,6 +77,7 @@ if __name__ == "__main__":
             if t == "base":
                 d["inputs"].append(problems[i]["base_input"][n])
             else:
+                print(task_id, n)
                 d["inputs"].append(new_inputs[task_id][n])
         with open("HumanEvalPlusInputsMin.jsonl", "a") as f:
             f.write(json.dumps(d) + "\n")
