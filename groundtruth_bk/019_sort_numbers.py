0ed20942ef1ab706a85b670bba60bb8d750fa598
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-04-25 09:52:07 -0500

    fix(contract): 019

--- groundtruth_bk/019_sort_numbers.py
+++ groundtruth_bk/019_sort_numbers.py
@@ -8,6 +8,7 @@ def sort_numbers(numbers: str) -> str:
     >>> sort_numbers('three one five')
     'one three five'
     """
+    assert isinstance(numbers, str), "invalid inputs" # $_CONTRACT_$
     assert numbers == "" or all(map(lambda x: x in ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], numbers.split(" "))), "invalid inputs" # $_CONTRACT_$
     
     to_int = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
