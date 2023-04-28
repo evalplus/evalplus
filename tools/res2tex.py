9a26cabc73fbaea75d7d9680318756f5d4959986
ganler <jaway.liu@gmail.com>
2023-04-27 20:04:53 -0500

    refactor: display greedy pass@1 in res2tex

--- tools/res2tex.py
+++ tools/res2tex.py
@@ -10,11 +10,50 @@ from termcolor import cprint
 
 from eval_plus.evaluation.evaluate import estimate_pass_at_k
 
+
+def analyze_resfile(resfile):
+    before_summary = {}
+    after_summary = {}
+
+    res = json.load(open(resfile))["eval"]
+    total = []
+    before_pass = []
+    after_pass = []
+    for v in res.values():
+        total.append(len(v["files"]))
+        bc = sum([r[0] == SUCCESS for r in v["base"]])
+        before_pass.append(bc)
+        if v["plus"]:
+            after_pass.append(
+                sum(
+                    [
+                        v["plus"][i][0] == v["base"][i][0] == SUCCESS
+                        for i in range(len(v["plus"]))
+                    ]
+                )
+            )
+
+    total = np.array(total)
+    before_pass = np.array(before_pass)
+    after_pass = np.array(after_pass)
+    for k in [1, 10, 100]:
+        if total.min() >= k:
+            pass_at_k = estimate_pass_at_k(total, before_pass, k).mean()
+            before_summary[f"pass@{k}"] = pass_at_k
+    for k in [1, 10, 100]:
+        if total.min() >= k:
+            pass_at_k = estimate_pass_at_k(total, after_pass, k).mean()
+            after_summary[f"pass@{k}"] = pass_at_k
+
+    return before_summary, after_summary
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--type", type=str, required=True)
     args = parser.parse_args()
 
+    # Analyszing 0.2~0.8 temperatures.
     resfiles = []
     TEMPS = [0.2, 0.4, 0.6, 0.8]
     # check existance
@@ -30,41 +69,24 @@ if __name__ == "__main__":
 
     for resfile in resfiles:
         # load the results
-        res = json.load(open(resfile))["eval"]
-        total = []
-        before_pass = []
-        after_pass = []
-        for v in res.values():
-            total.append(len(v["files"]))
-            bc = sum([r[0] == SUCCESS for r in v["base"]])
-            before_pass.append(bc)
-            if v["plus"]:
-                after_pass.append(
-                    sum(
-                        [
-                            v["plus"][i][0] == v["base"][i][0] == SUCCESS
-                            for i in range(len(v["plus"]))
-                        ]
-                    )
-                )
-
-        total = np.array(total)
-        before_pass = np.array(before_pass)
-        after_pass = np.array(after_pass)
-        for k in [1, 10, 100]:
-            if (total >= k).all():
-                pass_at_k = estimate_pass_at_k(total, before_pass, k).mean()
-                before_summary.setdefault(f"pass@{k}", []).append(pass_at_k)
-        for k in [1, 10, 100]:
-            if (total >= k).all():
-                pass_at_k = estimate_pass_at_k(total, after_pass, k).mean()
-                after_summary.setdefault(f"pass@{k}", []).append(pass_at_k)
+        before, after = analyze_resfile(resfile)
+        for k, v in before.items():
+            before_summary.setdefault(k, []).append(v)
+        for k, v in after.items():
+            after_summary.setdefault(k, []).append(v)
 
     # print pass@1~100, and corresponding max temperature
     print("Before")
     print(before_summary)
     print("After")
     print(after_summary)
+
+    # Analyszing greedy decoding (temperature=0.0)
+    gf = os.path.join(f"{args.type}_temp_0.0", f"eval_results.json")
+    assert os.path.exists(gf)
+    bfgreedy, afgreedy = analyze_resfile(gf)
+    bfgreedy = bfgreedy["pass@1"] * 100
+    afgreedy = afgreedy["pass@1"] * 100
 
     TEXTTEMPS = [r"\temptwo{}", r"\tempfour{}", r"\tempsix{}", r"\tempeight{}"]
 
@@ -84,6 +106,14 @@ if __name__ == "__main__":
 
     print("LaTeX Table Ingredent")
     argmax = [np.argmax(v) for v in before_summary.values()]
-    cprint("before & " + make_line(before_summary, argmax), "green")
+    cprint(
+        f"before & {bfgreedy:.1f} & " + make_line(before_summary, argmax),
+        "green",
+    )
     argmax = [np.argmax(v) for v in after_summary.values()]
-    cprint(aplus("after") + " & " + make_line(after_summary, argmax, ap=True), "green")
+    cprint(
+        aplus("after")
+        + f" & {aplus(f'{afgreedy:.1f}')} & "
+        + make_line(after_summary, argmax, ap=True),
+        "green",
+    )
