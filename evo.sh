36b9606997bcdb8a0e8217ad81e2672e9751278f
ganler <jaway.liu@gmail.com>
2023-04-27 19:26:32 -0500

    refactor: evo.sh supports gptneo and use -j to explicit re-run

--- evo.sh
+++ evo.sh
@@ -6,6 +6,7 @@ set -x
 # Set default values for DATADIR and NCORES
 DEFAULT_DATADIR="/JawTitan/EvalPlus/humaneval"
 DEFAULT_NCORES=$(nproc)
+DEFAULT_JUST_RUN=0
 
 # Check if the user has provided a custom value for DATADIR and NCORES
 while getopts ":d:n:" opt; do
@@ -15,6 +16,9 @@ while getopts ":d:n:" opt; do
       ;;
     n )
       NCORES=$OPTARG
+      ;;
+    j )
+      JUST_RUN=1
       ;;
     \? )
       echo "Invalid option: -$OPTARG. Example: bash evo.sh -d /path/to/humaneval -n 32" 1>&2
@@ -30,17 +34,28 @@ done
 # Set DATADIR and NCORES to default values if they are not provided by the user
 DATADIR=${DATADIR:-$DEFAULT_DATADIR}
 NCORES=${NCORES:-$DEFAULT_NCORES}
+JUST_RUN=${JUST_RUN:-$DEFAULT_JUST_RUN}
 
 export PYTHONPATH=$(pwd)
 
-models=("codegen-2b" "codegen-6b" "codegen-16b" "vicuna-7b" "vicuna-13b" "stablelm-7b" "incoder-1b" "incoder-6b" "polycoder" "chatgpt" "santacoder")
+models=("codegen-2b" "codegen-6b" "codegen-16b" "vicuna-7b" "vicuna-13b" "stablelm-7b" "incoder-1b" "incoder-6b" "polycoder" "chatgpt" "santacoder" "gptneo-2b")
 temps=("0.0" "0.2" "0.4" "0.6" "0.8")
+
+if [ $JUST_RUN -eq 0 ]; then
+  echo "Experiements won't run from scratch since JUST_RUN is set to 0. To run and override all experiements, add -j flag."
+else
+  echo "Experiements will run from scratch since JUST_RUN is set to 1. To run without overriding all experiements, remove -j flag."
+fi
 
 for model in "${models[@]}"; do
   for temp in "${temps[@]}"; do
     folder="${DATADIR}/${model}_temp_${temp}"
     if [ -d "$folder" ]; then
-      yes | python3 eval_plus/evaluation/evaluate.py --dataset humaneval --r_folder "$folder" --parallel ${NCORES} --i-just-wanna-run --extra --full
+      if [ $JUST_RUN -eq 1 ]; then
+        yes | python3 eval_plus/evaluation/evaluate.py --dataset humaneval --r_folder "$folder" --parallel ${NCORES} --i-just-wanna-run --extra --full
+      else
+        python3 eval_plus/evaluation/evaluate.py --dataset humaneval --r_folder "$folder" --parallel ${NCORES} --extra --full
+      fi
     else
       echo "Folder does not exist: $folder"
     fi
