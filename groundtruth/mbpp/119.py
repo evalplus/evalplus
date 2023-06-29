42d91349f23a1a0c6214b417993c9a7a101b7999
FatPigeorz <wjhhhhhha0@gmail.com>
2023-06-29 19:56:12 +0800

    contract[mbpp]: 117/118/119/120

--- groundtruth/mbpp/119.py
+++ groundtruth/mbpp/119.py
@@ -3,6 +3,9 @@ Write a python function to find the element that appears only once in a sorted a
 """
 
 def search(arr):
+    assert isinstance(arr, list), "invalid inputs" # $_CONTRACT_$
+    assert all(isinstance(x, int) for x in arr), "invalid inputs" # $_CONTRACT_$
+    assert all(a <= b for a, b in zip(arr, arr[1:])), "invalid inputs" # $_CONTRACT_$
     n = len(arr)
     XOR = 0
     for i in range(n) :
