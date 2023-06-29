e583ed2cc4d74fb1c79d6e1264d61070c4c7683f
FatPigeorz <wjhhhhhha0@gmail.com>
2023-06-29 17:19:55 +0800

    contract[mbpp]: 096/097/098/099

--- groundtruth/mbpp/096.py
+++ groundtruth/mbpp/096.py
@@ -3,6 +3,8 @@ Write a python function to find the number of divisors of a given integer.
 """
 
 def divisor(n):
+  assert isinstance(n, int), "invalid inputs" # $_CONTRACT_$
+  assert n > 0, "invalid inputs" # $_CONTRACT_$
   for i in range(n):
     x = len([i for i in range(1,n+1) if not n % i])
   return x
