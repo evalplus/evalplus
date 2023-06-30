2a481bada098a9a5dec327350413a04d49dddf7f
soryxie <1670167385@qq.com>
2023-06-30 13:39:21 +0800

    contract[mbpp]: 222/223/224/226

--- groundtruth/mbpp/222.py
+++ groundtruth/mbpp/222.py
@@ -3,12 +3,14 @@ Write a function to check if all the elements in tuple have same data type or no
 """
 
 def check_type(test_tuple):
-  res = True
-  for ele in test_tuple:
-    if not isinstance(ele, type(test_tuple[0])):
-      res = False
-      break
-  return (res) 
+    assert isinstance(test_tuple, tuple), "invalid inputs" # $_CONTRACT_$
+    assert len(test_tuple) > 0, "invalid inputs" # $_CONTRACT_$
+    res = True
+    for ele in test_tuple:
+        if not isinstance(ele, type(test_tuple[0])):
+        res = False
+        break
+    return (res) 
 
 
 
