ea956e0e9846507052f5e72855dbc258b1914874
soryxie <1670167385@qq.com>
2023-06-30 18:23:03 +0800

    contract[mbpp]: 266/267/268/269

--- groundtruth/mbpp/269.py
+++ groundtruth/mbpp/269.py
@@ -3,6 +3,8 @@ Write a function to find the ascii value of a character.
 """
 
 def ascii_value(k):
+  assert isinstance(k, str), "invalid inputs" # $_CONTRACT_$
+  assert len(k)==1, "invalid inputs" # $_CONTRACT_$
   ch=k
   return ord(ch)
 
