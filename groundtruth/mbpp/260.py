fdd731f2415c932c292b573ac69d7da5fb6984da
soryxie <1670167385@qq.com>
2023-06-30 16:51:09 +0800

    contract[mbpp]: 256/257/259/260

--- groundtruth/mbpp/260.py
+++ groundtruth/mbpp/260.py
@@ -3,6 +3,7 @@ Write a function to find the nth newman–shanks–williams prime number.
 """
 
 def newman_prime(n): 
+	assert isinstance(n, int), "invalid inputs" # $_CONTRACT_$
 	if n == 0 or n == 1: 
 		return 1
 	return 2 * newman_prime(n - 1) + newman_prime(n - 2)
