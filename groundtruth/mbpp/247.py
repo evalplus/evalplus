e7d5e3df604c0005a5a4fd259f848398e0b09bc8
soryxie <1670167385@qq.com>
2023-06-30 15:33:40 +0800

    contract[mbpp]: 247/248/249/250

--- groundtruth/mbpp/247.py
+++ groundtruth/mbpp/247.py
@@ -2,17 +2,18 @@
 Write a function to find the length of the longest palindromic subsequence in the given string.
 """
 
-def lps(str): 
-	n = len(str) 
+def lps(str1): 
+	assert isinstance(str1, str), "invalid inputs" # $_CONTRACT_$
+	n = len(str1) 
 	L = [[0 for x in range(n)] for x in range(n)] 
 	for i in range(n): 
 		L[i][i] = 1
 	for cl in range(2, n+1): 
 		for i in range(n-cl+1): 
 			j = i+cl-1
-			if str[i] == str[j] and cl == 2: 
+			if str1[i] == str1[j] and cl == 2: 
 				L[i][j] = 2
-			elif str[i] == str[j]: 
+			elif str1[i] == str1[j]: 
 				L[i][j] = L[i+1][j-1] + 2
 			else: 
 				L[i][j] = max(L[i][j-1], L[i+1][j]); 
--- groundtruth/mbpp/249.py
+++ groundtruth/mbpp/249.py
@@ -3,8 +3,10 @@ Write a function to find the intersection of two arrays.
 """
 
 def intersection_array(array_nums1,array_nums2):
- result = list(filter(lambda x: x in array_nums1, array_nums2)) 
- return result
+    assert isinstance(array_nums1, list), "invalid inputs" # $_CONTRACT_$
+    assert isinstance(array_nums2, list), "invalid inputs" # $_CONTRACT_$
+    result = list(filter(lambda x: x in array_nums1, array_nums2)) 
+    return result
 
 
 
