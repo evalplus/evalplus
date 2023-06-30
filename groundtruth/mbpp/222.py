0eb43eda6c29042f2df1e65e21d010ad7b0ed228
FatPigeorz <wjhhhhhha0@gmail.com>
2023-06-30 14:43:22 +0800

    contract[mbpp]: 222

--- groundtruth/mbpp/222.py
+++ groundtruth/mbpp/222.py
@@ -8,8 +8,8 @@ def check_type(test_tuple):
     res = True
     for ele in test_tuple:
         if not isinstance(ele, type(test_tuple[0])):
-        res = False
-        break
+            res = False
+            break
     return (res) 
 
 
