2bfd20ad2e50fe5f1f9b252e28b950d4eeeb0412
soryxie <1670167385@qq.com>
2023-06-30 14:02:12 +0800

    contract[mbpp]: 227/228/229/230

--- groundtruth/mbpp/230.py
+++ groundtruth/mbpp/230.py
@@ -2,9 +2,12 @@
 Write a function that takes in a string and character, replaces blank spaces in the string with the character, and returns the string.
 """
 
-def replace_blank(str1,char):
- str2 = str1.replace(' ', char)
- return str2
+def replace_blank(str1, char):
+    assert isinstance(str1, str), "invalid inputs" # $_CONTRACT_$
+    assert isinstance(char, str), "invalid inputs" # $_CONTRACT_$
+    assert len(char) == 1, "invalid inputs" # $_CONTRACT_$
+    str2 = str1.replace(' ', char)
+    return str2
 
 
 
