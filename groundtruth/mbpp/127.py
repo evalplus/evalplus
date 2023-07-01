410a932afedb2a0568aa5ef68b2df83aa35e7c1e
ganler <jaway.liu@gmail.com>
2023-07-01 02:21:46 -0500

    contract[mbpp]: simplify 127.py impl

--- groundtruth/mbpp/127.py
+++ groundtruth/mbpp/127.py
@@ -5,14 +5,7 @@ Write a function to multiply two integers.
 def multiply_int(x, y):
     assert isinstance(x, int), "invalid inputs" # $_CONTRACT_$
     assert isinstance(y, int), "invalid inputs" # $_CONTRACT_$
-    if y < 0:
-        return -multiply_int(x, -y)
-    elif y == 0:
-        return 0
-    elif y == 1:
-        return x
-    else:
-        return x + multiply_int(x, y - 1)
+    return x * y
 
 
 
