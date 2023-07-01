3e153c67538da47c2cd3e75942a20e88e0400ced
soryxie <1670167385@qq.com>
2023-07-01 13:34:31 +0800

    contract[mbpp]: 293/294/295/296

--- groundtruth/mbpp/293.py
+++ groundtruth/mbpp/293.py
@@ -4,6 +4,8 @@ Write a function to find the third side of a right angled triangle.
 
 import math
 def otherside_rightangle(w,h):
+  assert isinstance(w, (int, float)) and w > 0, "invalid inputs" # $_CONTRACT_$
+  assert isinstance(h, (int, float)) and h > 0, "invalid inputs" # $_CONTRACT_$
   s=math.sqrt((w*w)+(h*h))
   return s
 
