9ff4fd361dc4a340d30d37f5d0649b4b43d33462
ganler <jaway.liu@gmail.com>
2023-07-01 02:13:56 -0500

    contract[mbpp]: optimize 100.py

--- groundtruth/mbpp/100.py
+++ groundtruth/mbpp/100.py
@@ -2,14 +2,13 @@
 Write a function to find the next smallest palindrome of a specified integer, returned as an integer.
 """
 
-import sys
 def next_smallest_palindrome(num):
     assert isinstance(num, int), "invalid inputs" # $_CONTRACT_$
     assert num >= 0, "invalid inputs" # $_CONTRACT_$
-    numstr = str(num)
-    for i in range(num+1,sys.maxsize):
-        if str(i) == str(i)[::-1]:
-            return i
+    ret = num + 1
+    while not str(ret) == str(ret)[::-1]:
+        ret += 1
+    return ret
 
 
 
