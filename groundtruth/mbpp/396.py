72902e8644a9f38a009a2535531d6db0ab1f7c99
soryxie <1670167385@qq.com>
2023-07-02 16:29:23 +0800

    contract[mbpp]: 395/396/397/398

--- groundtruth/mbpp/396.py
+++ groundtruth/mbpp/396.py
@@ -5,6 +5,7 @@ Write a function to check whether the given string starts and ends with the same
 import re  
 regex = r'^[a-z]$|^([a-z]).*\1$'
 def check_char(string): 
+	assert isinstance(string, str), "invalid inputs" # $_CONTRACT_$
 	if(re.search(regex, string)): 
 		return "Valid" 
 	else: 
--- groundtruth/mbpp/398.py
+++ groundtruth/mbpp/398.py
@@ -3,6 +3,7 @@ Write a function to compute the sum of digits of each number of a given list.
 """
 
 def sum_of_digits(nums):
+    assert isinstance(nums, list), "invalid inputs" # $_CONTRACT_$
     return sum(int(el) for n in nums for el in str(n) if el.isdigit())
 
 
