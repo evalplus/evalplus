e0452304f39f2c2fa2cb4d44f90146c7e252991b
soryxie <1670167385@qq.com>
2023-07-02 19:01:02 +0800

    contract[mbpp]: 456/457/458/459

--- groundtruth/mbpp/458.py
+++ groundtruth/mbpp/458.py
@@ -3,6 +3,8 @@ Write a function to find the area of a rectangle.
 """
 
 def rectangle_area(l,b):
+  assert isinstance(l, (int, float)), "invalid inputs" # $_CONTRACT_$
+  assert isinstance(b, (int, float)), "invalid inputs" # $_CONTRACT_$
   area=l*b
   return area
 
