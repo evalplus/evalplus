f03249027856a67e14b51630935c765712a7fda9
FatPigeorz <wjhhhhhha0@gmail.com>
2023-07-02 17:13:12 +0800

    contract[mbpp]: 605

--- groundtruth/mbpp/605.py
+++ groundtruth/mbpp/605.py
@@ -4,7 +4,6 @@ Write a function to check if the given integer is a prime number.
 
 def prime_num(num):
   assert isinstance(num, int), "invalid inputs" # $_CONTRACT_$
-  assert num >= 0, "invalid inputs" # $_CONTRACT_$
   if num >=1:
    for i in range(2, num//2):
      if (num % i) == 0:
