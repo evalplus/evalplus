db53a38e06627e2fa48aba23b4aa398ec55b5203
FatPigeorz <wjhhhhhha0@gmail.com>
2023-07-02 22:14:10 +0800

    contract[mbpp]: 736/737/738/739

--- groundtruth/mbpp/736.py
+++ groundtruth/mbpp/736.py
@@ -4,6 +4,9 @@ Write a function to locate the left insertion point for a specified value in sor
 
 import bisect
 def left_insertion(a, x):
+    assert isinstance(a, list), "invalid inputs" # $_CONTRACT_$
+    assert all(x <= y for x, y in zip(a, a[1:])), "invalid inputs" # $_CONTRACT_$
+    assert all(isinstance(x, (int, float)) for x in a), "invalid inputs" # $_CONTRACT_$
     i = bisect.bisect_left(a, x)
     return i
 
