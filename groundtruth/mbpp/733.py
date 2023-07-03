47336f5404a282208e4673a5ffb7548b8dd65c04
FatPigeorz <wjhhhhhha0@gmail.com>
2023-07-03 16:23:19 +0800

    contract[mbpp]: fix 733.py testcase

--- groundtruth/mbpp/733.py
+++ groundtruth/mbpp/733.py
@@ -24,4 +24,4 @@ def find_first_occurrence(A, x):
 
 assert find_first_occurrence([2, 5, 5, 5, 6, 6, 8, 9, 9, 9], 5) == 1
 assert find_first_occurrence([2, 3, 5, 5, 6, 6, 8, 9, 9, 9], 5) == 2
-assert find_first_occurrence([2, 4, 1, 5, 6, 6, 8, 9, 9, 9], 6) == 4
+assert find_first_occurrence([1, 2, 4, 5, 6, 6, 8, 9, 9, 9], 6) == 4
