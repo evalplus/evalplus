57023c984c4eee93da49b43af81c8f837c6e40a6
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-07-04 08:38:14 -0500

    contract[mbpp]: fix 082

--- groundtruth/mbpp/082.py
+++ groundtruth/mbpp/082.py
@@ -5,7 +5,7 @@ Write a function to find the volume of a sphere.
 import math
 def volume_sphere(r):
   assert isinstance(r, (int, float)), "invalid inputs" # $_CONTRACT_$
-  assert r >= 0, "invalid inputs" # $_CONTRACT_$
+  assert r > 0, "invalid inputs" # $_CONTRACT_$
   volume=(4/3)*math.pi*r*r*r
   return volume
 
