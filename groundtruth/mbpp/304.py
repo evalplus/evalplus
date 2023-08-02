338c5a0073657ec125ae692c21b2aab210d29a9c
soryxie <1670167385@qq.com>
2023-08-02 20:59:27 +0800

    contract[mbpp]: fix 262/267/282/304

--- groundtruth/mbpp/304.py
+++ groundtruth/mbpp/304.py
@@ -5,7 +5,8 @@ Write a python function to find element at a given index after number of rotatio
 def find_Element(arr,ranges,rotations,index) :  
     assert isinstance(arr, list), "invalid inputs" # $_CONTRACT_$
     assert isinstance(ranges, list), "invalid inputs" # $_CONTRACT_$
-    assert all(isinstance(x, list) for x in ranges), "invalid inputs" # $_CONTRACT_$
+    assert all(isinstance(x, list) and len(x) == 2 for x in ranges), "invalid inputs" # $_CONTRACT_$
+    assert all(isinstance(xitem, int) for x in ranges for xitem in x), "invalid inputs" # $_CONTRACT_$
     assert isinstance(rotations, int) and rotations > 0, "invalid inputs" # $_CONTRACT_$
     assert isinstance(index, int) and index >= 0, "invalid inputs" # $_CONTRACT_$
     for i in range(rotations - 1,-1,-1 ) : 
