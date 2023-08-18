0d1235d50df4691376b9d8c6e09ec7a2ac9a7f67
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-08-18 12:49:44 +0800

    fix: recover unexpected overwriting

--- tools/humaneval/fix_v015.py
+++ tools/humaneval/fix_v015.py
@@ -5,19 +5,35 @@ def check_id(data, task_id):
     assert data[task_id]["task_id"] == f"HumanEval/{task_id}"
 
 
+def check_valid(s: str):
+    cnt = 0
+    for ch in s:
+        if ch == "(":
+            cnt += 1
+        elif ch == ")":
+            cnt -= 1
+        else:
+            return False
+        if cnt < 0:
+            return False
+    return cnt == 0
+
+
 def fix(data):
     check_id(data, 126)
-
     data[126]["contract"] = (
         '\n    assert type(lst) == list, "invalid inputs" # $_CONTRACT_$'
         + '\n    assert all(type(x) == int and x >= 0 for x in lst), "invalid inputs" # $_CONTRACT_$\n'
     )
-
     data[126]["plus_input"] = [
         l
         for l in data[126]["plus_input"]
         if type(l[0]) == list and all(type(x) == int and x >= 0 for x in l[0])
     ]
+
+    check_id(data, 6)
+    data[6]["contract"] += '    assert cnt == 0, "invalid inputs"\n'
+    data[6]["plus_input"] = [l for l in data[6]["plus_input"] if check_valid(l[0])]
 
     return data
 
