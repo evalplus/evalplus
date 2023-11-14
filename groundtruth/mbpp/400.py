5d652502735b5c2995b4527d93228ebc9ae48f85
soryxie <1670167385@qq.com>
2023-11-14 06:26:50 -0600

    fix[mbpp] 400: fix problematic groundtruth

--- groundtruth/mbpp/400.py
+++ groundtruth/mbpp/400.py
@@ -5,10 +5,10 @@ Write a function to extract the number of unique tuples in the given list.
 def extract_freq(test_list):
   assert isinstance(test_list, list), "invalid inputs" # $_CONTRACT_$
   assert all(isinstance(ele, tuple) for ele in test_list), "invalid inputs" # $_CONTRACT_$
-  return len(set(tuple(sorted(l)) for l in test_list))
+  return len(set(tuple(l) for l in test_list))
 
 
 
-assert extract_freq([(3, 4), (1, 2), (4, 3), (5, 6)] ) == 3
+assert extract_freq([(3, 4), (1, 2), (4, 3), (5, 6)] ) == 4
 assert extract_freq([(4, 15), (2, 3), (5, 4), (6, 7)] ) == 4
 assert extract_freq([(5, 16), (2, 3), (6, 5), (6, 9)] ) == 4
