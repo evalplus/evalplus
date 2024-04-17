1aad1667a7fb2956a885ceccbbee875b31de3a7f
soryxie <1670167385@qq.com>
2024-04-16 23:45:28 -0500

    fix: merge into one version

--- tools/mbpp/fix_v010.py
+++ tools/mbpp/fix_v010.py
@@ -100,17 +100,76 @@ def fix(data):
         "https://www.geeksforgeeks.org/sum-of-all-odd-length-subarrays/", ""
     )
 
-    check_id(data, 375, "Mbpp/783")
-    data[375]["prompt"] = data[375]["prompt"].replace(
-        "https://www.geeksforgeeks.org/program-change-rgb-color-model-hsv-color-model/",
-        "",
-    )
-
     check_id(data, 392, "Mbpp/803")
     data[392]["prompt"] = data[392]["prompt"].replace(
         "https://www.geeksforgeeks.org/check-if-given-number-is-perfect-square-in-cpp/",
         "",
     )
+
+    # fix: https://github.com/evalplus/evalplus/issues/147
+
+    check_id(data, 375, "Mbpp/783")
+    del data[375]
+
+    check_id(data, 345, "Mbpp/746")
+    del data[345]
+
+    check_id(data, 318, "Mbpp/640")
+    del data[318]
+
+    check_id(data, 282, "Mbpp/595")
+    del data[282]
+
+    check_id(data, 270, "Mbpp/582")
+    del data[270]
+
+    check_id(data, 263, "Mbpp/574")
+    del data[263]
+
+    check_id(data, 231, "Mbpp/461")
+    del data[231]
+
+    check_id(data, 216, "Mbpp/442")
+    del data[216]
+
+    check_id(data, 212, "Mbpp/438")
+    del data[212]
+
+    check_id(data, 206, "Mbpp/431")
+    del data[206]
+
+    check_id(data, 187, "Mbpp/407")
+    del data[187]
+
+    check_id(data, 183, "Mbpp/400")
+    del data[183]
+
+    check_id(data, 180, "Mbpp/396")
+    del data[180]
+
+    check_id(data, 160, "Mbpp/295")
+    del data[160]
+
+    check_id(data, 121, "Mbpp/249")
+    del data[121]
+
+    check_id(data, 107, "Mbpp/229")
+    del data[107]
+
+    check_id(data, 94, "Mbpp/164")
+    del data[94]
+
+    check_id(data, 89, "Mbpp/143")
+    del data[89]
+
+    check_id(data, 67, "Mbpp/117")
+    del data[67]
+
+    check_id(data, 65, "Mbpp/115")
+    del data[65]
+
+    check_id(data, 37, "Mbpp/83")
+    del data[37]
 
     return data
 
@@ -136,7 +195,6 @@ if __name__ == "__main__":
         "Mbpp/780",
         "Mbpp/781",
         "Mbpp/782",
-        "Mbpp/783",
         "Mbpp/803",
     ]
     SOURCE_VERSION = "v0.1.0"
